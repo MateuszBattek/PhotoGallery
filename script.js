04d58c4eddb6369392a14a11f7d9b8a6b5635fa3
Mateusz Battek <mateuszbattek@gmail.com>
2023-01-30 19:11:25 +0100

    fully working beautiful gallery

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const modalImage = document.getElementById("modal-image");
 const modalImageImg = document.getElementById("modal-image-img");
 const galleryContainer = document.getElementById("gallery-container");
 const currentImages = document.getElementsByClassName("current-image");
+const galleries = document.getElementsByClassName("gallery");
 
 const displayImage = (src) => {
     modalImage.style.display = "block";
@@ -13,6 +14,29 @@ modalImage.addEventListener('click', function (e) {
         modalImage.style.display = "none";
     }
 });
+
+let gallery_index = 0;
+for (let gallery of galleries) {
+    for (let i = 1; i <= 10; i++) {
+        let photo_index = gallery_index * 10 + i;
+
+        let gallery_item = document.createElement("div");
+        gallery_item.classList.add("gallery__item");
+        gallery_item.classList.add("gallery__item--" + i);
+
+        gallery_item.onclick = () => displayImage("./gallery-pictures/" + i + "w.jpg");
+
+        let gallery_img = document.createElement("img");
+        gallery_img.classList.add("gallery__img");
+        gallery_img.src = "min-pictures/" + photo_index + "w.jpg";
+        gallery_img.alt = "Image " + photo_index;
+
+        gallery_item.appendChild(gallery_img);
+        gallery.appendChild(gallery_item);
+    }
+
+    gallery_index++;
+}
 
 // let random_photos = [];
 // while (random_photos.length < 8) {
