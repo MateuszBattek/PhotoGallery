62fb1d3f1ca820480a857d77f9286c374b5c2bf7
Mateusz Battek <mateuszbattek@gmail.com>
2023-02-07 19:06:59 +0100

    kind of animated slider

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const modalImage = document.getElementById("modal-image");
 const modalImageImg = document.getElementById("modal-image-img");
 const galleryContainer = document.getElementById("gallery-container");
-const currentImages = document.getElementsByClassName("current-image");
+const imageContainers = document.getElementsByClassName("image-container");
 const galleries = document.getElementsByClassName("gallery");
 const toggleMenuButton = document.getElementById('toggle-menu');
 const menu = document.getElementById('menu');
+const sliderImagesSection = document.getElementById("slider-images-section");
+
+let sliderImages = [];
+for (let imageContainer of imageContainers) {
+    sliderImages.push(imageContainer.children[0]);
+}
 
 const displayImage = (src) => {
     modalImage.style.display = "flex";
@@ -41,28 +47,27 @@ for (let gallery of galleries) {
     gallery_index++;
 }
 
-let images = [];
-for (let i = 1; i <= 5; i++) {
-    images.push("./slider-pictures/zdjecie" + i + ".jpg");
-}
-let currentImagesIndexes = [0, 1, 2];
+let currentSliderTransform = 0;
+let currentCenterPhoto = 1;
 
 function prevImage() {
-    currentImagesIndexes = currentImagesIndexes.map(index =>
-        index > 0 ? index - 1 : 4
-    );
-
-    for (let i = 0; i < currentImagesIndexes.length; i++) {
-        currentImages[i].src = images[currentImagesIndexes[i]];
+    if (currentCenterPhoto === 0) return;
+    sliderImages[currentCenterPhoto].classList.remove("center-image");
+    currentCenterPhoto = currentCenterPhoto <= 0 ? currentCenterPhoto = 4 : currentCenterPhoto - 1;
+    sliderImages[currentCenterPhoto].classList.add("center-image");
+    currentSliderTransform += 27;
+    for (let i = 0; i < imageContainers.length; i++) {
+        imageContainers[i].style = `transform: translate(${currentSliderTransform}vw, 0);`;
     }
 }
 function nextImage() {
-    currentImagesIndexes = currentImagesIndexes.map(index =>
-        index < 4 ? index + 1 : 0
-    );
-
-    for (let i = 0; i < currentImagesIndexes.length; i++) {
-        currentImages[i].src = images[currentImagesIndexes[i]];
+    if (currentCenterPhoto === 4) return;
+    sliderImages[currentCenterPhoto].classList.remove("center-image");
+    currentCenterPhoto = currentCenterPhoto >= 4 ? currentCenterPhoto = 0 : currentCenterPhoto + 1;
+    sliderImages[currentCenterPhoto].classList.add("center-image");
+    currentSliderTransform -= 27;
+    for (let i = 0; i < imageContainers.length; i++) {
+        imageContainers[i].style = `transform: translate(${currentSliderTransform}vw, 0);`;
     }
 }
 
